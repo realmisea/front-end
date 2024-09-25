a561fb2cdf72bf472f1f2a551e313f3c9ef5db70
zzzzzuuuuu <agu0830@naver.com>
2024-09-25 23:24:00 +0900

    chore: path 별칭 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    resolve: {
+        alias: [
+            { find: '@components', replacement: '/src/components' },
+            { find: '@apis', replacement: '/src/apis' },
+            { find: '@pages', replacement: '/src/pages' },
+            { find: '@assets', replacement: '/src/assets' }
+        ]
+    }
 })
