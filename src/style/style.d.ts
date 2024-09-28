81be6d2a53146dd68771f7231a317ce26bb6b666
zzzzzuuuuu <agu0830@naver.com>
2024-09-28 21:34:21 +0900

    style: 공통 색상 정의 및 ThemeProvider 설정

---
+++ src/style/style.d.ts
@@ -0,0 +1,11 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+        colors: {
+            yellow: string;
+            white: string;
+            black: string;
+        };
+    }
+}
---
+++ src/style/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'styled-components';
+
+const Theme: DefaultTheme = {
+    colors: {
+        yellow: '#FFEC8A',
+        white: '#FFEC8A',
+        black: '#1D1D1D'
+    }
+};
+
+export { Theme };
