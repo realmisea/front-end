839e70dfe36f9ef79bd26060c7a9f35e99b44ad1
zzzzzuuuuu <agu0830@naver.com>
2024-10-10 22:13:41 +0900

    refactor: 헤더 적용을 위한 태그 감쌈

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import {MainPage} from "./pages/main";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {MainPage} from "@pages/main";
+import {Header} from "@components/Header.tsx";
 
 function App() {
 
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<MainPage />} />
-        </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                </Routes>
+            </BrowserRouter>
+            <Header />
+        </>
+    )
 }
 
 export default App
