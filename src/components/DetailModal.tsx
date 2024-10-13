7dbf70ecda66d78df4c9537f4f8363ba3c539740
zzzzzuuuuu <agu0830@naver.com>
2024-10-13 22:34:17 +0900

    feat: 시간별 날씨 컴포넌트 UI 구현

--- src/components/DetailModal.tsx
+++ src/components/DetailModal.tsx
@@ -1,6 +1,7 @@
 import {CloseBtn, HeaderTitle, ModalContainer, ModalHeaderContainer} from "./SuggestionModal.tsx";
 import Close from '@assets/images/close-button.svg';
 import styled from "styled-components";
+import {HourWeather} from "./HourWeather.tsx";
 
 export const DetailModal = () => {
 
@@ -13,7 +14,13 @@ export const DetailModal = () => {
             <ModalBodyContainer>
                 <WeatherContainer>
                     <SubTitle>시간별 예보</SubTitle>
-                    <DetailContainer>날씨띄움</DetailContainer>
+                    <DetailContainer>
+                        <HourWeather />
+                        <HourWeather />
+                        <HourWeather />
+                        <HourWeather />
+                        <HourWeather />
+                    </DetailContainer>
                 </WeatherContainer>
                 <AirContainer>
                     <SubTitle>통합 대기</SubTitle>
@@ -55,7 +62,11 @@ const SubTitle = styled.h2`
   margin: 15px 0 15px 0;
 `
 const DetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   width: 100%;
   height: 100%;
   background: lightpink;
+  gap: 3px;
 `
