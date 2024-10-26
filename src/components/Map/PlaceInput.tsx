b925a3526530d0835f06a03dd2b3d69cca2c7616
zzzzzuuuuu <agu0830@naver.com>
2024-10-26 22:50:51 +0900

    feat: map page 출발, 도착 지점 컨테이너 UI 구현

---
+++ src/components/Map/PlaceInput.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import SearchIcon from '@assets/images/map/search.svg';
+
+interface PlaceInputProps {
+  title: string;
+}
+
+export const PlaceInput = ({ title }: PlaceInputProps) => {
+  console.log(title);
+  return (
+    <InputContainer>
+      <InputText>{title}</InputText>
+      <SearchImg src={SearchIcon} />
+    </InputContainer>
+  );
+};
+
+const InputContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 357px;
+  height: 55px;
+  background-color: ${({ theme }) => theme.colors.yellow};
+  border-radius: 20px;
+`;
+
+const InputText = styled.p`
+  font-size: 24px;
+  margin-left: 27px;
+`;
+
+const SearchImg = styled.img`
+  margin-right: 27px;
+  cursor: pointer;
+`;
--- src/pages/map/map.tsx
+++ src/pages/map/map.tsx
@@ -1,3 +1,40 @@
+import { PlaceInput } from '@components/Map/PlaceInput.tsx';
+import Blue from '@assets/images/map/blue-circle.svg';
+import Arrow from '@assets/images/map/connection-arrow.svg';
+import Red from '@assets/images/map/red-circle.svg';
+import styled from 'styled-components';
+
 export const MapPage = () => {
-  return <div>안뇽</div>;
+  return (
+    <MapContainer>
+      <MarkContainer>
+        <img src={Blue} />
+        <img src={Arrow} />
+        <img src={Red} />
+      </MarkContainer>
+      <PlaceContainer>
+        <PlaceInput title={'출발지: '} />
+        <PlaceInput title={'도착지: '} />
+      </PlaceContainer>
+    </MapContainer>
+  );
 };
+
+const MapContainer = styled.div`
+  display: flex;
+  gap: 9px;
+`;
+
+const MarkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 4px;
+`;
+
+const PlaceContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 56px;
+  background: forestgreen;
+`;
